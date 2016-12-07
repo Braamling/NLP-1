b010f45a80a9d7ebc1b7046cbe40006ed974f559
Bram van den Akker <git@bramvandenakker.nl>
2016-12-07 20:53:15 +0100

    comments in the utils file

--- bram_sandbox/utils.py
+++ bram_sandbox/utils.py
@@ -37,92 +37,7 @@ class Vocab(object):
     def __len__(self):
         return len(self.word_freq)
 
-def calculate_perplexity(log_probs):
-    # https://web.stanford.edu/class/cs124/lec/languagemodeling.pdf
-    perp = 0
-    for p in log_probs:
-        perp += -p
-    return np.exp(perp / len(log_probs))
-
-def get_multi_hot(ingredients, ingredient_list):
-    multi_hot = np.zeros(len(ingredient_list))
-
-    for ingredient in ingredients:
-        multi_hot[ingredient.values()[0]] = 0
-
-    return multi_hot
-
-
-'''
-[
-    recipe {
-        ingredients: []
-        steps: [{sentence: line1, "...": "..." }, {sentence: line1}, {sentence: line1}]
-    }
-
-]
-'''
-def get_ingredient_list_size(dict_fn):
-    return len(load_pickle_to_dict(dict_fn))    
-
-def get_words_from_dataset(fn):
-    with open(fn) as recipe_file:    
-        recipes = json.load(recipe_file)
-
-        for recipe in recipes:
-            recipe = recipe['steps']
-            for step in recipe:
-                for word in step['sentence'].split():
-                    yield word
-            yield '<endofrecipe>'
-
-def get_dataset(fn, dict_fn, vocab, number_of_steps, batch_size):
-    ingredient_list = load_pickle_to_dict(dict_fn)
-    recipes = []
-    with open(fn) as recipe_file:    
-        recipes_json = json.load(recipe_file)
-
-        for recipe in recipes_json:
-            ingredient_multi_hot = get_multi_hot(recipe['ingredients'], ingredient_list)
-
-            # Create one hot vectors for each word in the recipe 
-            recipe = [vocab.encode(word) for word in yield_words(recipe['steps'])]
-            recipe = np.array(recipe)
-            recipes.append(Recipe(number_of_steps, ingredient_multi_hot, recipe))
-
-    return [batch for batch in recipe_iterator(recipes, batch_size, number_of_steps)]
-
-def yield_words(recipe):
-    for step in recipe:
-        for word in step['sentence'].split():
-            yield word
-        yield '<endofrecipe>'
-
-def load_pickle_to_dict(fn):
-    return pickle.load(open(fn, 'rb'))
-
-def recipe_iterator(recipes, batch_size, number_of_steps):
-    padding_size = batch_size - (len(recipes) % batch_size)
-
-    # pad the recipes with empty recipes
-    for i in range(padding_size):
-        recipes.append(Recipe(number_of_steps))
-    
-    # Create batches for each set of reripces
-    for recipes in np.split(np.asarray(recipes), batch_size):
-        yield RecipeBatch(recipes)
-
-
-def sample(a, temperature=1.0):
-        # helper function to sample an index from a probability array
-        # from https://github.com/fchollet/keras/blob/master/examples/lstm_text_generation.py
-        a = np.log(a) / temperature
-        a = np.exp(a) / np.sum(np.exp(a))
-        return np.argmax(np.random.multinomial(1, a, 1))
-
-
 class Recipe():
-
     def __init__(self, number_of_steps, multihot=None, words=None):
         if multihot is None:
             self.sequences_x = []
@@ -133,9 +48,16 @@ class Recipe():
             self.number_of_steps = number_of_steps
             self.create_sequences(number_of_steps)
 
+    """
+    Get the multihot ingredient list of this recipe
+    """
     def get_multihot(self):
         return self.multihot
 
+    """
+    Create the sequences of length "number_of_steps" and pad the last one with
+    zeros
+    """
     def create_sequences(self, number_of_steps):
         padding_size = number_of_steps - (len(self.words) % number_of_steps)
 
@@ -145,9 +67,17 @@ class Recipe():
         self.sequences_x = np.split(words[:-1], number_of_steps)
         self.sequences_y = np.split(words[1:], number_of_steps)
 
+    """
+    Get the actual amount of sequences of length "number_of_steps" of this recipe
+    """
     def get_sequences_amount(self):
         return len(self.sequences_y)
 
+
+    """
+    Get the sequence x and y on position i. two zero arrays of length 
+    "number_of_steps" is returned if the index is out of range.
+    """
     def get_sequence_i(self, index):
         if index >= (self.sequences_x):
             return np.zeros(number_of_steps), np.zeros(number_of_steps)
@@ -155,15 +85,22 @@ class Recipe():
             return self.sequences_x[index], self.sequences_y[index]
 
 class RecipeBatch():
+    """
+    A recipe batch consists of N Recipe object that can be used for iteration.
+    """
 
     def __init__(self, recipes):
         self.recipes = recipes
         self.max_seq = 0
 
+        # Find the histest amount of sequences in the batch.
         for recipe in recipes:
             if recipe.get_sequences_amount() > self.max_seq:
                 self.max_seq = recipe.get_sequences_amount()
 
+    """
+    Retrieve a tupel with two all x and y sequences of this batch
+    """
     def get_all_sequence_i(self, index):
         batch_x = []
         batch_y = []
@@ -175,6 +112,9 @@ class RecipeBatch():
         
         return (batch_x, batch_y)
 
+    """
+    Retrieve an array with all multihots of all sequences in this batch
+    """
     def get_all_multihots(self):
         multihots = []
 
@@ -183,8 +123,108 @@ class RecipeBatch():
 
         return multihots
 
+    """
+    Get the largest sequence size in this batch
+    """
     def get_max_sequence_size(self):
         return self.max_seq
 
+
+def calculate_perplexity(log_probs):
+    # https://web.stanford.edu/class/cs124/lec/languagemodeling.pdf
+    perp = 0
+    for p in log_probs:
+        perp += -p
+    return np.exp(perp / len(log_probs))
+
+"""
+Return a multihot list based on the ingredient_list of a recipe
+"""
+def get_multi_hot(ingredients, ingredient_list):
+    multi_hot = np.zeros(len(ingredient_list))
+
+    # Set all the multihot indecies to 1 of existing ingredients.
+    for ingredient in ingredients:
+        multi_hot[ingredient.values()[0]] = 1
+
+    return multi_hot
+
+def get_ingredient_list_size(dict_fn):
+    return len(load_pickle_to_dict(dict_fn))    
+
+"""
+Yield all words in a dataset
+"""
+def get_words_from_dataset(fn):
+    with open(fn) as recipe_file:    
+        recipes = json.load(recipe_file)
+
+        for recipe in recipes:
+            recipe = recipe['steps']
+            for step in recipe:
+                for word in step['sentence'].split():
+                    yield word
+            yield '<endofrecipe>'
+
+"""
+Retrieve that dataset as a list of Recipe batches.
+"""
+def get_dataset(fn, dict_fn, vocab, number_of_steps, batch_size):
+    ingredient_list = load_pickle_to_dict(dict_fn)
+    recipes = []
+    with open(fn) as recipe_file:    
+        recipes_json = json.load(recipe_file)
+
+        # Create a recipe object for each recipe.
+        for recipe in recipes_json:
+            ingredient_multi_hot = get_multi_hot(recipe['ingredients'], ingredient_list)
+
+            # Create one hot vectors for each word in the recipe 
+            recipe = [vocab.encode(word) for word in yield_words(recipe['steps'])]
+            recipe = np.array(recipe)
+            recipes.append(Recipe(number_of_steps, ingredient_multi_hot, recipe))
+
+    # Create batches of recipes to later iterate
+    recipe_batches = [batch for batch in
+                      recipe_iterator(recipes, batch_size, number_of_steps)]
+
+    return recipe_batches
+
+"""
+Iterate over all given recipes and yield batches of recipes in a RecipeBatch object
+"""
+def recipe_iterator(recipes, batch_size, number_of_steps):
+    padding_size = batch_size - (len(recipes) % batch_size)
+
+    # pad the recipes with empty recipes
+    for i in range(padding_size):
+        recipes.append(Recipe(number_of_steps))
+    
+    # Create batches for each set of reripces
+    for recipes in np.split(np.asarray(recipes), batch_size):
+        yield RecipeBatch(recipes)
+
+"""
+Yield all the words in a recipe into a single sequence.
+"""
+def yield_words(recipe):
+    for step in recipe:
+        for word in step['sentence'].split():
+            yield word
+        yield '<endofrecipe>'
+
+"""
+Load a pickle file in dict form into memory
+"""
+def load_pickle_to_dict(fn):
+    return pickle.load(open(fn, 'rb'))
+
+
+def sample(a, temperature=1.0):
+        # helper function to sample an index from a probability array
+        # from https://github.com/fchollet/keras/blob/master/examples/lstm_text_generation.py
+        a = np.log(a) / temperature
+        a = np.exp(a) / np.sum(np.exp(a))
+        return np.argmax(np.random.multinomial(1, a, 1))
 
 
